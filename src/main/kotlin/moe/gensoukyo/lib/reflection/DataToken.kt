0e17b7cc5896ef1ad416fcbe25b3fb3813900930
ChloePrime <chloe_von_einzbern@qq.com>
2021-05-12 10:18:46 +0800

    添加DataToken的语法糖（IData.get/put(DataToken<...>)）

---
+++ src/main/kotlin/moe/gensoukyo/lib/reflection/DataToken.kt
@@ -0,0 +1,50 @@
+@file:Suppress("Unused")
+
+/**
+ * DataToken的语法糖
+ */
+package moe.gensoukyo.lib.reflection
+
+import moe.gensoukyo.lib.constants.ModIds
+import moe.gensoukyo.lib.maps.DataToken
+import net.minecraftforge.fml.common.Optional
+import noppes.npcs.api.entity.data.IData
+
+// CNPC IData
+
+@Optional.Method(modid = ModIds.CNPC)
+fun <T> IData.put(key: DataToken<T>, value: T) {
+    key.put(this, value)
+}
+
+@Optional.Method(modid = ModIds.CNPC)
+fun <T> IData.get(key: DataToken<T>): T {
+    return key.get(this)
+}
+
+@Optional.Method(modid = ModIds.CNPC)
+fun <T> IData.remove(key: DataToken<T>) {
+    key.clear(this)
+}
+
+@Optional.Method(modid = ModIds.CNPC)
+fun <T> IData.has(key: DataToken<T>): Boolean {
+    return key.peek(this) != null
+}
+
+// Plain Java Maps
+
+fun <T> Map<String, Any>.put(key: DataToken<T>, value: T) {
+    key.put(this, value)
+}
+
+fun <T> Map<String, Any>.get(key: DataToken<T>): T {
+    return key.get(this)
+}
+
+fun <T> Map<String, Any>.remove(key: DataToken<T>) {
+    key.clear(this)
+}
+fun <T> Map<String, Any>.has(key: DataToken<T>): Boolean {
+    return key.peek(this) != null
+}
