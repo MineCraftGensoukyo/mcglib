1f8382226aafb6ab6409abfd74074f526c215b72
ChloePrime <chloe_von_einzbern@qq.com>
2021-05-14 16:16:48 +0800

    Add: 给予临时op的API

---
+++ src/main/kotlin/moe/gensoukyo/lib/server/permission/PermissionUtils.kt
@@ -0,0 +1,37 @@
+@file:Suppress("unused")
+
+package moe.gensoukyo.lib.server.permission
+
+import net.minecraftforge.fml.common.FMLCommonHandler
+import net.minecraftforge.fml.relauncher.Side
+import noppes.npcs.api.entity.IPlayer
+
+/**
+ * 给予临时op
+ */
+inline fun IPlayer<*>.withOp(block: IPlayer<*>.() -> Unit) {
+    if (FMLCommonHandler.instance().side == Side.CLIENT) {
+        block()
+        return
+    }
+    val playerManager = this.mcEntity.server.playerList
+    val profile = this.mcEntity.gameProfile
+    val isOpBefore = playerManager.canSendCommands(profile)
+
+    if (!isOpBefore) {
+        playerManager.addOp(profile)
+    }
+    try {
+        block()
+    } finally {
+        if (!isOpBefore) {
+            playerManager.removeOp(profile)
+        }
+    }
+}
+
+fun IPlayer<*>.executeCommand(cmd: String): Int {
+    return this.mcEntity.server.commandManager.executeCommand(
+        this.mcEntity, cmd
+    )
+}
