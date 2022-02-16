ebce2b5212ff22d39dd06a419148a349ee71472e
ChloePrime <chloe_von_einzbern@qq.com>
2022-02-16 18:59:06 +0800

    修复单人模式下命令永远没权限的 bug

--- src/main/kotlin/moe/gensoukyo/lib/internal/common/command/PermissionNodeBasedCommand.kt
+++ src/main/kotlin/moe/gensoukyo/lib/internal/common/command/PermissionNodeBasedCommand.kt
@@ -4,6 +4,7 @@ import net.minecraft.command.CommandBase
 import net.minecraft.command.ICommandSender
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.server.MinecraftServer
+import net.minecraftforge.fml.common.FMLCommonHandler
 import net.minecraftforge.server.permission.PermissionAPI
 
 abstract class PermissionNodeBasedCommand(
@@ -14,6 +15,10 @@ abstract class PermissionNodeBasedCommand(
         sender: ICommandSender
     ): Boolean {
         if (sender !is EntityPlayer) return true
+        // 在物理客户端下，以世界是否允许作弊作为鉴权依据，而不是权限节点。
+        if (FMLCommonHandler.instance().side.isClient) {
+            return sender.world.worldInfo.areCommandsAllowed()
+        }
         return PermissionAPI.hasPermission(sender, permission)
     }
 }
