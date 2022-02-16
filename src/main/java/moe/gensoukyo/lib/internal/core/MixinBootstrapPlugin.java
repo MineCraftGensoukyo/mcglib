9da48fe2f5eab3695da8319138ae5c76b4651b14
ChloePrime <chloe_von_einzbern@qq.com>
2022-02-16 18:53:01 +0800

    更新 MixinBootstrapPlugin 实现，添加一个是否需要 Hack Classpath 的常量

--- src/main/java/moe/gensoukyo/lib/internal/core/MixinBootstrapPlugin.java
+++ src/main/java/moe/gensoukyo/lib/internal/core/MixinBootstrapPlugin.java
@@ -1,6 +1,7 @@
 package moe.gensoukyo.lib.internal.core;
 
 import net.minecraft.launchwrapper.Launch;
+import net.minecraftforge.fml.relauncher.FMLLaunchHandler;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin.Name;
 import org.apache.commons.lang3.reflect.MethodUtils;
@@ -17,6 +18,7 @@ import java.util.Map;
  */
 @Name("MCGLib MixinBootstrapPlugin")
 public class MixinBootstrapPlugin implements IFMLLoadingPlugin {
+    private static final boolean NEED_TO_HACK_OTHER_MOD = true;
 
     @SuppressWarnings("unused")
     public static void initMixin() {
@@ -28,6 +30,10 @@ public class MixinBootstrapPlugin implements IFMLLoadingPlugin {
      * Adds any .jar in `mods` folder to LaunchClassLoader.
      */
     private static void fixJvmMagic(File myJar) {
+        if (FMLLaunchHandler.isDeobfuscatedEnvironment()) {
+            // no-op in dev mode.
+            return;
+        }
         File[] mods = myJar.getParentFile().listFiles();
         if (mods == null) {
             throw new ExceptionInInitializerError("Cannot find mods folder!");
@@ -46,7 +52,9 @@ public class MixinBootstrapPlugin implements IFMLLoadingPlugin {
 
     @Override
     public void injectData(@Nonnull Map<String, Object> data) {
-        fixJvmMagic((File) data.get("coremodLocation"));
+        if (NEED_TO_HACK_OTHER_MOD) {
+            fixJvmMagic((File) data.get("coremodLocation"));
+        }
         try {
             ClassLoader appClassLoader = Launch.class.getClassLoader();
             MethodUtils.invokeMethod(appClassLoader, true, "addURL", this.getClass().getProtectionDomain().getCodeSource().getLocation());
