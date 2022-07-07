76de16c1fb2f3afea4a1b29e7813becdec213676
MrMks <i0991958@163.com>
2022-07-07 23:08:47 +0800

    1.12.3 进一步处理 mixin 和 mod 间的加载顺序关系

--- src/main/java/moe/gensoukyo/lib/internal/core/MixinBootstrapPlugin.java
+++ src/main/java/moe/gensoukyo/lib/internal/core/MixinBootstrapPlugin.java
@@ -3,6 +3,9 @@ package moe.gensoukyo.lib.internal.core;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin.Name;
 import org.spongepowered.asm.launch.MixinBootstrap;
+import org.spongepowered.asm.service.IMixinService;
+import org.spongepowered.asm.service.ITransformerProvider;
+import org.spongepowered.asm.service.MixinService;
 
 import javax.annotation.Nonnull;
 import java.util.Map;
@@ -28,10 +31,21 @@ public class MixinBootstrapPlugin implements IFMLLoadingPlugin {
 
     @Override
     public String[] getASMTransformerClass() {
-        return new String[] {
+        return excludeDelegate(new String[] {
                 "moe.gensoukyo.lib.internal.core.NpcInterfaceTransformer"
-        };
+        });
     }
+
+    // 防止重入
+    private static String[] excludeDelegate(String[] ary) {
+        IMixinService service = MixinService.getService();
+        ITransformerProvider itp = service == null ? null : service.getTransformerProvider();
+        if (itp != null) {
+            for (String str : ary) itp.addTransformerExclusion("$wrapper.".concat(str));
+        }
+        return ary;
+    }
+
 
     @Override
     public String getModContainerClass() {
